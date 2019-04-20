2f1a4131c8e519643857ba0fdee527791193e3f0
mattmorgan6 <mattmorgan6@gmail.com>
2019-04-20 14:37:18 -0700

    The login page now has entrys without borders and looks very good!

---
+++ KMSCalendar/KMSCalendar.Android/MyEntryRenderer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using KMSCalendar.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using KMSCalendar;
+
+[assembly: ExportRenderer(typeof(MyEntry), typeof(MyEntryRenderer))]
+namespace KMSCalendar.Droid
+{
+    class MyEntryRenderer : EntryRenderer
+    {
+        public MyEntryRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if(Control != null)
+            {
+                Control.Background = null;
+            }
+        }
+    }
+}
---
+++ KMSCalendar/KMSCalendar.UWP/MyEntryRenderer.cs
@@ -0,0 +1,28 @@
+﻿using KMSCalendar;
+using KMSCalendar.UWP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml.Media;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.UWP;
+
+[assembly: ExportRenderer(typeof(MyEntry), typeof(MyEntryRenderer))]
+namespace KMSCalendar.UWP
+{
+    public class MyEntryRenderer : EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if(Control != null)
+            {
+                Control.BorderThickness = new Windows.UI.Xaml.Thickness(0);
+            }
+        }
+    }
+}
