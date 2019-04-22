7885aa2dd9448e86bc9a23ca86617a493de051d4
mattmorgan6 <mattmorgan6@gmail.com>
2019-04-22 13:38:28 -0700

    Added a sign up page, it is not complete though. Still, the app builds and runs with no errors.

---
+++ KMSCalendar/KMSCalendar/Views/SignUpPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace KMSCalendar.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class SignUpPage : ContentPage
+	{
+		public SignUpPage ()
+		{
+			InitializeComponent ();
+		}
+
+        private void AlreadyUserButton_Clicked(object sender, EventArgs e)
+        {
+            //do something with the sign up info.
+        }
+
+        private void AuthenticateSignUpButton_Clicked(object sender, EventArgs e)
+        {
+            //redirect to log in page.
+        }
+    }
+}
