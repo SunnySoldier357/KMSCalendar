484b9326aeeb7ccdeef5b9690f741edff94cb76d
mattmorgan6 <mattmorgan6@gmail.com>
2019-04-23 09:22:26 -0700

    Added new view model for sign up. it is incomplete

---
+++ KMSCalendar/KMSCalendar/ViewModels/SignUpViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace KMSCalendar.ViewModels
+{
+    public class SignUpViewModel : INotifyPropertyChanged
+    {
+        //* Event Handlers
+        public void OnNotifyPropertyChanged(string property) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+
+        //* Private Methods
+        private void modifyProperty<T>(ref T value, ref T privateProperty, string nameOfProperty)
+        {
+            if (!value.Equals(privateProperty))
+            {
+                privateProperty = value;
+                OnNotifyPropertyChanged(nameOfProperty);
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
