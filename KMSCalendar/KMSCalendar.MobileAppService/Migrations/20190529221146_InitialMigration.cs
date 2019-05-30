c5f94da6b0419ce8928a500dc010dfd1e48792be
Sandeep Singh Sidhu <sandeep01@hotmail.sg>
2019-05-29 17:23:21 -0700

    [MobileAppService] Add InitialMigration Migration

---
+++ KMSCalendar/KMSCalendar.MobileAppService/Migrations/20190529221146_InitialMigration.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace KMSCalendar.MobileAppService.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Assignments",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    DueDate = table.Column<DateTime>(nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Assignments", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Assignments");
+        }
+    }
+}
