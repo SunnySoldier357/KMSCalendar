a6d173c192b29b771f2fbd19253e226296ed4daa
Sandeep Singh Sidhu <sandeep01@hotmail.sg>
2019-06-01 16:25:37 -0700

    [Xamarin.Forms] Use ModelValidation NuGet package for Validation

--- KMSCalendar/KMSCalendar/ViewModels/LogInViewModel.cs
+++ KMSCalendar/KMSCalendar/ViewModels/LogInViewModel.cs
@@ -1,19 +1,21 @@
-﻿using System.ComponentModel;
+﻿using ModelValidation;
 
 namespace KMSCalendar.ViewModels
 {
-    public class LogInViewModel : INotifyPropertyChanged
+    public class LogInViewModel : ValidatableObject
     {
         //* Private Properties
         private string email;
         private string loginValidationMessage;
         private string password;
-        private string confirmPassword;
-        private string userName;
 
         //* Public Properties
         public int LogInAttempts { get; set; }
 
+        [ContainsCharacter('@')]
+        [DoesNotContainCharacter(' ')]
+        [MinimumLength(5)]
+        [MaximumLength(254)]
         public string Email
         {
             get => email;
@@ -21,28 +23,23 @@ namespace KMSCalendar.ViewModels
         }
         public string LoginValidationMessage
         {
-            get => loginValidationMessage;
+            get
+            {
+                if (Errors != null && Errors.Count > 0)
+                    return Errors[0];
+
+                return loginValidationMessage;  
+            }
             set => modifyProperty(ref value, ref loginValidationMessage,
                 nameof(LoginValidationMessage));
         }
+        [MinimumLength(8)]
+        [MaximumLength(64)]
         public string Password
         {
             get => password;
             set => modifyProperty(ref value, ref password, nameof(Password));
         }
-        public string ConfirmPassword
-        {
-            get => confirmPassword;
-            set => modifyProperty(ref value, ref confirmPassword, nameof(ConfirmPassword));
-        }
-        public string UserName
-        {
-            get => userName;
-            set => modifyProperty(ref value, ref userName, nameof(UserName));
-        }
-
-        //* Events
-        public event PropertyChangedEventHandler PropertyChanged;
 
         //* Constructor
         public LogInViewModel()
@@ -50,83 +47,12 @@ namespace KMSCalendar.ViewModels
             Email = string.Empty;
             LoginValidationMessage = string.Empty;
             Password = string.Empty;
-            ConfirmPassword = string.Empty;
-            UserName = string.Empty;
-        }
 
-        //* Public Methods
-        public bool IsLoginModelValid()
-        {
-            LoginValidationMessage = string.Empty;
-
-            bool result = validateProperty(ref email, nameof(Email), 5, 254);
-
-            if (result && Email.Contains(" "))
+            PropertyChanged += (sender, args) =>
             {
-                LoginValidationMessage = "No spaces in email please";
-                result = false;
-            }
-            if (result && !Email.Contains("@"))
-            {
-                LoginValidationMessage = "Please use valid email";
-                result = false;
-            }
-
-            if (result)
-                result = validateProperty(ref password, nameof(Password), 8, 64);
-
-            return result;
-        }
-
-        public bool IsSignUpModelValid()
-        {
-            LoginValidationMessage = string.Empty;
-
-            bool result = validateProperty(ref userName, nameof(UserName), 2, 64);
-
-            if (result)
-                result = IsLoginModelValid();
-
-            if (result && (Password != ConfirmPassword))
-            {
-                LoginValidationMessage = "Passwords do not match";
-                result = false;
-            }
-
-            return result;
-        }
-
-        //* Event Handlers
-        public void OnNotifyPropertyChanged(string property) =>
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
-
-        //* Private Methods
-        private bool invalidModel(string validationMessage)
-        {
-            LoginValidationMessage = validationMessage;
-            return false;
-        }
-
-        private void modifyProperty<T>(ref T value, ref T privateProperty, string nameOfProperty)
-        {
-            if (!value.Equals(privateProperty))
-            {
-                privateProperty = value;
-                OnNotifyPropertyChanged(nameOfProperty);
-            }
-        }
-
-        private bool validateProperty(ref string property, string nameOfProperty,
-            int minimum, int maximum)
-        {
-            if (string.IsNullOrWhiteSpace(property))
-                return invalidModel($"No {nameOfProperty}");
-            else if (property.Length < minimum)
-                return invalidModel($"{nameOfProperty} Too Short");
-            else if (property.Length > maximum)
-                return invalidModel($"{nameOfProperty} Too Long");
-
-            return true;
+                if (args.PropertyName == nameof(Errors))
+                    OnNotifyPropertyChanged(nameof(LoginValidationMessage));
+            };
         }
     }
 }
