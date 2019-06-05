17ae9cfb6f2d2d9fb2924e3dc442b38ff466ef14
Sandeep Singh Sidhu <sandeep01@hotmail.sg>
2019-06-05 06:14:09 -0700

    [Xamarin.Forms] Add SettingsViewModel

---
+++ KMSCalendar/KMSCalendar/ViewModels/SettingsViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System.Windows.Input;
+
+using Xamarin.Forms;
+
+using KMSCalendar.Models;
+using KMSCalendar.Views;
+
+namespace KMSCalendar.ViewModels
+{
+    public class SettingsViewModel : BaseViewModel
+    {
+        //* Private Properties
+        private Settings settings = Settings.DefaultInstance;
+
+        //* Public Properties
+        public bool IsDarkThemeEnabled
+        {
+            get => settings.Theme == Theme.Dark;
+            set
+            {
+                settings.Theme = value ? Theme.Dark : Theme.Light;
+                OnPropertyChanged();
+            }
+        }
+        public bool ShowCalendarDays
+        {
+            get => settings.ShowCalendarDays;
+            set
+            {
+                settings.ShowCalendarDays = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand LogOutCommand { get; set; }
+
+        //* Constructors
+        public SettingsViewModel()
+        {
+            Title = "Settings";
+
+            LogOutCommand = new Command(() => ExecuteLogOutCommand());
+        }
+
+        //* Public Methods
+        public void ExecuteLogOutCommand()
+        {
+            App app = Application.Current as App;
+
+            settings.SignedInUserId = null;
+            app.SignedInUser = null;
+
+            app.MainPage = new LoginPage();
+        }
+    }
+}
--- KMSCalendar/KMSCalendar/Views/SettingsPage.xaml.cs
+++ KMSCalendar/KMSCalendar/Views/SettingsPage.xaml.cs
@@ -1,42 +1,12 @@
 ﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-using KMSCalendar.Models;
-
 namespace KMSCalendar.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class SettingsPage : ContentPage
     {
-        //* Private Properties
-        public Settings settings = Settings.DefaultInstance;
-
         //* Constructor
-        public SettingsPage()
-        {
-            InitializeComponent();
-
-            ThemeSwitch.IsToggled = settings.Theme == Theme.Dark;
-            CalendarDaySwitch.IsToggled = settings.ShowCalendarDays;
-
-            BindingContext = (Application.Current as App).SignedInUser;
-        }
-
-        //* Event Handlers
-        private void CalendarDaySwitch_Toggled(object sender, ToggledEventArgs e) =>
-            settings.ShowCalendarDays = CalendarDaySwitch.IsToggled;
-
-        private void LogOutButton_Clicked(object sender, System.EventArgs e)
-        {
-            App app = Application.Current as App;
-
-            settings.SignedInUserId = null;
-            app.SignedInUser = null;
-
-            app.MainPage = new LoginPage();
-        }
-
-        private void ThemeSwitch_Toggled(object sender, ToggledEventArgs e) => 
-            settings.Theme = ThemeSwitch.IsToggled ? Theme.Dark : Theme.Light;
+        public SettingsPage() => InitializeComponent();
     }
 }
