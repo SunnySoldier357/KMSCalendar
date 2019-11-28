66d2ec6e25cba05860374a7579273b76edcaf698
mattmorgan6 <mattmorgan6@gmail.com>
2019-11-27 16:29:48 -0800

    Connection required to the database.

---
+++ KMSCalendar/KMSCalendar/Services/Connections.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KMSCalendar.Services
+{
+    public class Connections
+    {
+        private static string ConnectionString = "<Insert Connection String Here>";
+
+        public static string GetConnectionString() => ConnectionString;
+    }
+}
