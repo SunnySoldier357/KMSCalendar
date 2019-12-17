4a9eb68c736863e90638f819f6891f7f5858a4d1
Sandeep Singh Sidhu <sandeep01@hotmail.sg>
2019-12-17 18:50:03 +0800

    Make all data models a subclass of TableData

--- KMSCalendar/KMSCalendar/Models/Data/Assignment.cs
+++ KMSCalendar/KMSCalendar/Models/Data/Assignment.cs
@@ -3,7 +3,7 @@
 namespace KMSCalendar.Models.Data
 {
     /// <summary>A class representing a particular assignment.</summary>
-    public class Assignment
+    public class Assignment : TableData
     {
         //* Public Properties
         public Class Class { get; set; }
@@ -16,7 +16,9 @@ namespace KMSCalendar.Models.Data
         /// repetitive so that Dapper can put it in the db
         /// </summary>
         public Guid ClassId { get; set; }
-        public Guid Id { get; set; }
+        /// <summary>The user id of the assignment creator</summary>
+        public Guid UserId { get; set; }
+
         /// <summary>The period of the class that the user belongs to.
         /// This is repetitive so that Dapper can put it in the db.
         /// </summary>
@@ -26,12 +28,10 @@ namespace KMSCalendar.Models.Data
         public string Description { get; set; }
         /// <summary>The name of the assignment.</summary>
         public string Name { get; set; }
-        /// <summary>The user id of the assignment creator</summary>
-        public Guid UserId { get; set; }
 
         //* Public Methods
         public void SetClassId() => ClassId = Class.Id;
-        
+
         public void SetPeriod() => Period = Class.Period;
     }
 }
--- KMSCalendar/KMSCalendar/Models/Data/User.cs
+++ KMSCalendar/KMSCalendar/Models/Data/User.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 
 namespace KMSCalendar.Models.Data
 {
-    public class User
+    public class User : TableData
     {
         //* Public Properties
-        public Guid Id { get; set; }
         public Guid SchoolId { get; set; }
+
         public List<Class> EnrolledClasses { get; set; }
+
         public string Email { get; set; }
         public string Password { get; set; }
         public string UserName { get; set; }
