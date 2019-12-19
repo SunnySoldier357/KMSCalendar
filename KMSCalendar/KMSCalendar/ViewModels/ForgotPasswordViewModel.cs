5d1a77c7df8d1c15e72d0c614346481b1c7e3e5c
mattmorgan6 <mattmorgan6@gmail.com>
2019-12-19 14:56:19 -0800

    Added verificataion and new password views. They are not currently functional.

--- KMSCalendar/KMSCalendar/ViewModels/ForgotPasswordViewModel.cs
+++ KMSCalendar/KMSCalendar/ViewModels/ForgotPasswordViewModel.cs
@@ -10,6 +10,9 @@ namespace KMSCalendar.ViewModels
     {
         private string email;
         private string validationMessage;
+        private bool emailVisibility;
+        private bool verificationVisibility;
+        private bool newPasswordVisibility;
 
         public string Email
         {
@@ -21,19 +24,52 @@ namespace KMSCalendar.ViewModels
             get => validationMessage;
             set => setProperty(ref validationMessage, value);
         }
+        public bool VerificationVisibility
+        {
+            get => verificationVisibility;
+            set => setProperty(ref verificationVisibility, value);
+        }
+        public bool EmailVisibility
+        {
+            get => emailVisibility;
+            set => setProperty(ref emailVisibility, value);
+        }
+        public bool NewPasswordVisibility
+        {
+            get => newPasswordVisibility;
+            set => setProperty(ref newPasswordVisibility, value);
+        }
 
         public ICommand AuthenticateEmailCommand { get; }
         public ICommand GoBackCommand { get; }
 
         public ForgotPasswordViewModel()
         {
+            EmailVisibility = true;
+            VerificationVisibility = false;
             AuthenticateEmailCommand = new Command(() => AuthenticateEmail());
             GoBackCommand = new Command(() => (Application.Current as App).MainPage.Navigation.PopModalAsync());
         }
-
+        
         private void AuthenticateEmail()
         {
+            if (EmailVisibility)
+            {
+                EmailVisibility = false;
+                VerificationVisibility = true;
+            }
 
+            else if (VerificationVisibility)
+            {
+                VerificationVisibility = false;
+                NewPasswordVisibility = true;
+            }
+
+            else if (NewPasswordVisibility)
+            {
+                NewPasswordVisibility = false;
+                EmailVisibility = true;
+            }
         }
     }
 }
