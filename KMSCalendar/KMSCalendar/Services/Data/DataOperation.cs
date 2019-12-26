a85402b163531b59e5e1aa96c4e89d86f21f5f6e
mattmorgan6 <mattmorgan6@gmail.com>
2019-12-26 11:20:01 -0800

    Data operation now uses generics and dynamics.

--- KMSCalendar/KMSCalendar/Services/Data/DataOperation.cs
+++ KMSCalendar/KMSCalendar/Services/Data/DataOperation.cs
@@ -11,11 +11,19 @@ namespace KMSCalendar.Services.Data
 {
     public class DataOperation
     {
-        private Func<string, User> function;  //function
-        private string param;  //parameter
-        private User data;  //data
+        private dynamic function;
+        private dynamic param;
+        private dynamic data;
 
-        public async Task<User> ConnectToBackendAsync<T2, T1>(Func<string, User> function, string param)
+        /// <summary>
+        /// Attempts to get data from the backend, if it fails, it opens a network fail page.
+        /// </summary>
+        /// <typeparam name="T1">The type of param. (i.e. Guid)</typeparam>
+        /// <typeparam name="T2">The return type of the function.</typeparam>
+        /// <param name="function">The function used to get data from the backend.</param>
+        /// <param name="param">The parameter passed into the backend function.</param>
+        /// <returns>The data retrieved from the database.</returns>
+        public async Task<T2> ConnectToBackendAsync<T1, T2>(Func<T1, T2> function, T1 param)
         {
             this.function = function;
             this.param = param;
@@ -38,38 +46,6 @@ namespace KMSCalendar.Services.Data
             return data;
         }
 
-
-
-        //Generic version that does not work yet:
-
-        //public async Task<T2> ConnectToBackendAsync<T2, T1>(Func<T1, T2> function, T1 param)
-        //{
-        //    this.function = function;
-        //    this.param = param;
-        //    r = function.Method.ReturnType;
-
-        //   data = function(param);
-
-        //    if (data != null)
-        //        return data;
-
-
-        //    //Waits until the Network FailPage is closed before continuing
-        //    var waitHandle = new EventWaitHandle(false, EventResetMode.AutoReset);
-        //    var modalPage = new NetworkFailPage(this);
-        //    modalPage.Disappearing += (sender2, e2) =>              //Found this on stack overflow, still trying to undersand this line.
-        //    {
-        //        waitHandle.Set();
-        //    };
-        //    await (Application.Current as App).MainPage.Navigation.PushModalAsync(modalPage);
-        //    System.Diagnostics.Debug.WriteLine("The modal page is now on screen, hit back button");
-        //    await Task.Run(() => waitHandle.WaitOne());
-        //    System.Diagnostics.Debug.WriteLine("The modal page is dismissed, do something now");
-
-        //    return data;
-        //}
-
-
         /// <summary>
         /// Attempts to get data from the database, updates the data field if possible.
         /// </summary>
@@ -80,11 +56,8 @@ namespace KMSCalendar.Services.Data
             {
                 data = function(param);
                 return true;
-            }
-            catch
-            {
-                return false;
-            }
+            } 
+            catch { return false; }
         }
     }
 }
