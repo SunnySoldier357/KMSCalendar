c519e8557f57f44af7bf188dfe06bc1c6d482bcd
mattmorgan6 <mattmorgan6@gmail.com>
2020-01-04 12:17:47 -0800

    move signup views into one folder

---
+++ KMSCalendar/KMSCalendar/Views/SignUpPage/NewSchoolView.xaml.cs
@@ -0,0 +1,12 @@
+﻿
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace KMSCalendar.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class NewSchoolView : ContentView
+	{
+		public NewSchoolView() => InitializeComponent();
+	}
+}
---
+++ KMSCalendar/KMSCalendar/Views/SignUpPage/SignUpPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace KMSCalendar.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class SignUpPage : ContentPage
+	{
+		//* Constructors
+		public SignUpPage() => InitializeComponent();
+	}
+}
