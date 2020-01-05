f61859de709d1fc486e59d8561db06942e097b10
Sandeep Singh Sidhu <sandeep01@hotmail.sg>
2020-01-05 19:53:09 +0800

    Update Tables without publishing

--- CalendarDB/dbo/Tables/Class_Periods.sql
+++ CalendarDB/dbo/Tables/Class_Periods.sql
@@ -2,5 +2,6 @@
 (
 	[ClassId] UNIQUEIDENTIFIER NOT NULL,
     [Period] INT NOT NULL,
+    CONSTRAINT [PK_Class_Periods] PRIMARY KEY CLUSTERED (ClassId, [Period]),
     CONSTRAINT [FK_Class_Periods_ToClasses] FOREIGN KEY (ClassId) REFERENCES Classes(Id)
 )
--- CalendarDB/dbo/Tables/Class_Users.sql
+++ CalendarDB/dbo/Tables/Class_Users.sql
@@ -2,7 +2,8 @@
 (
 	[ClassId] UNIQUEIDENTIFIER NOT NULL,
     [UserId] UNIQUEIDENTIFIER NOT NULL,
-    [Period] INT NULL,
+    [Period] INT NOT NULL,
+    CONSTRAINT [PK_Class_Users] PRIMARY KEY CLUSTERED (ClassId, UserId, [Period]),
     CONSTRAINT [FK_Class_Users_ToClasses] FOREIGN KEY (ClassId) REFERENCES Classes(Id),
     CONSTRAINT [FK_Class_Users_ToUsers] FOREIGN KEY (UserId) REFERENCES Users(Id)
 )
