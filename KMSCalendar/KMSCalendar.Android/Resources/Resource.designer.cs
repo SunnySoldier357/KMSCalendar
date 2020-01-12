e02c3f52f555ba55cb9d9a5b6998e5ab6e583b7f
mattmorgan6 <mattmorgan6@gmail.com>
2020-01-11 17:13:31 -0800

    Updated app name and icon

--- KMSCalendar/KMSCalendar.Android/Resources/Resource.designer.cs
+++ KMSCalendar/KMSCalendar.Android/Resources/Resource.designer.cs
@@ -3675,6 +3675,9 @@ namespace KMSCalendar.Droid
 			// aapt resource value: 0x7F050067
 			public const int tooltip_background_light = 2131034215;
 			
+			// aapt resource value: 0x7F050068
+			public const int trans = 2131034216;
+			
 			static Color()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
@@ -6092,13 +6095,22 @@ namespace KMSCalendar.Droid
 		{
 			
 			// aapt resource value: 0x7F0C0000
-			public const int icon = 2131492864;
+			public const int HW_Icon = 2131492864;
 			
 			// aapt resource value: 0x7F0C0001
-			public const int icon_round = 2131492865;
+			public const int icon = 2131492865;
 			
 			// aapt resource value: 0x7F0C0002
-			public const int launcher_foreground = 2131492866;
+			public const int icon1 = 2131492866;
+			
+			// aapt resource value: 0x7F0C0003
+			public const int icon2 = 2131492867;
+			
+			// aapt resource value: 0x7F0C0004
+			public const int icon_round = 2131492868;
+			
+			// aapt resource value: 0x7F0C0005
+			public const int launcher_foreground = 2131492869;
 			
 			static Mipmap()
 			{
